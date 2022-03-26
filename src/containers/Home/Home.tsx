a0ade0f8942bd11b2369baeec6ea5bcb73f091d1
NhatTai20 <ntai20111@gmail.com>
2022-03-26 17:12:08 +0700

    test commit on new branch

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import './Home.scss';
 const Loading = () => (
   <div >
-    <h5>Loading...</h5>
+    <h5>Loading....</h5>
   </div>
 )
 export const Home = () => {
